fe97c3c1b5a885cf4112c9456a7b42ccd85b4159
80sVectorz <krijn_p@k-elf.nl>
2023-03-14 11:25:00 +0100

    Added base class deffitions. Made small intermediate test.

--- RoadRep.py
+++ RoadRep.py
@@ -1,1 +1,24 @@
 # RoadRep.py Main module
+import numpy as np
+from enum import Enum
+
+class RoadShapeType(Enum):
+    LINEAR = 0
+    CURVED = 1
+
+
+class RoadShape:
+    def __init__(self, shapeType: RoadShapeType) -> None:
+        self.shapeType = shapeType
+    def SetLinear(self, start: np.ndarray, end: np.ndarray) -> None:
+        if self.shapeType != RoadShapeType.LINEAR:
+            raise Exception(f"SetLINEAR requires a road shape type of LINEAR | type found: {self.shapeType}")
+
+class Lane:
+    def __init__(self,start: np.ndarray,end: np.ndarray,road) -> None:
+        self.start = start
+        self.end = end
+
+class Road:
+    def __init__(self,nLanes: int, shape) -> None:
+        pass
--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
 # Test.py testing file
+import colored_traceback
+colored_traceback.add_hook()
+
+from numpy import array as nparray;
+import RoadRep
+from RoadRep import RoadShapeType
+
+shape = RoadRep.RoadShape(RoadShapeType.CURVED)
+shape.SetLinear(nparray([1,2]),nparray([3,4]))
