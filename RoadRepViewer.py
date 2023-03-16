72203bab6ff95509e3a55eac1d417e5e1334526e
80sVectorz <krijn_p@k-elf.nl>
2023-03-16 14:20:21 +0100

    Added zoom feature Which had some required changes and additions.

--- RoadRepViewer.py
+++ RoadRepViewer.py
@@ -1,5 +1,6 @@
 # RoadRepViewer.py A simple view application used for debugging.
 # made using this template: https://gist.github.com/MatthewJA/7544830
+
 import RoadRep
 import sys
 import numpy as np
@@ -7,7 +8,17 @@ from Utils import Vector2
  
 import pygame
 from pygame.locals import *
- 
+
+# Function definitions:
+def WorldToScreen(p: np.ndarray) -> np.ndarray:
+  return (p*(Vector2(width,height)/frustum)*Vector2(1,-1)) + Vector2(width/2,height/2)
+
+def ScreenToWorld(p: np.ndarray,camPos: np.ndarray) -> np.ndarray:
+  return (p - Vector2(width/2,height/2))/((Vector2(width,height)/frustum)*Vector2(1,-1))+camPos
+
+def UpdateFrustum(zoom: int) -> np.ndarray:
+  return Vector2(zoom,zoom/screenRatio)
+
 pygame.init()
 
 fps = 60
@@ -15,51 +26,64 @@ fpsClock = pygame.time.Clock()
  
 width, height = 640, 480
 screenRatio = width/height
-frustum = Vector2(500,500/screenRatio)
 screen = pygame.display.set_mode((width, height))
 
-def PointToScreen(p: np.ndarray):
-  return (p*(Vector2(width,height)/frustum)*Vector2(1,-1)) + Vector2(width/2,height/2)
 
-#Test road network:
+# Test road network:
 shape = RoadRep.RoadShape(RoadRep.RoadShapeType.LINEAR)
 shape.SetLinear(Vector2(0,0),Vector2(50,40))
 roads = [RoadRep.Road(4,shape)]
 rn = RoadRep.RoadNet(roads)
 
-#runtime variables:
-camPos = Vector2(0,0)
+# runtime variables:
+zoom = 500
+frustum = Vector2(zoom,zoom/screenRatio)
+camPos = Vector2(0.0,0.0)
 panning = False
-panStartPos = Vector2(0,0)
+panStartPos = Vector2(0.0,0.0)
 panStartCamPos = camPos
 
-# program loop.
+# program loop:
 while True:
   screen.fill((0, 0, 0))
-  
+
+  # Event handling & Updates:
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
       sys.exit() 
-  # Update.
+    # Zooming:
+    if event.type == pygame.MOUSEWHEEL:
+      scrollChange = event.y
+      mousePos = pygame.mouse.get_pos()
+      mouseBeforePos = ScreenToWorld(np.array(mousePos),camPos)
+      zoom-=scrollChange*10
+      zoom=max(zoom,10)
+      frustum = UpdateFrustum(zoom)
+      mouseAfterPos = ScreenToWorld(mousePos,camPos)
+      camPos-=mouseBeforePos-mouseAfterPos
+
+  # Panning:
   mouseButton = pygame.mouse.get_pressed(3)[0]
   if mouseButton and not panning:
     panStartCamPos = camPos
-    panStartPos = np.array(pygame.mouse.get_pos())
+    print("camPos:",camPos)
+    panStartPos = ScreenToWorld(np.array(pygame.mouse.get_pos()),panStartCamPos)
     panning = True
   elif mouseButton and panning:
-    camPos=panStartCamPos+(np.array(pygame.mouse.get_pos())-panStartPos)
+    camPos=panStartCamPos+(ScreenToWorld(np.array(pygame.mouse.get_pos()),panStartCamPos)-panStartPos)
   elif not mouseButton and panning:
     panning = False
   
-  # Draw.
+  
+  # Draw:
   for road in rn.roads:
     for lane in road.lanes:
       if lane.shape.shapeType == RoadRep.RoadShapeType.LINEAR:
         pygame.draw.line(screen,
                          (0,255,0),
-                         PointToScreen(lane.shape.start)+camPos,
-                         PointToScreen(lane.shape.end)+camPos)
+                         WorldToScreen(lane.shape.start+camPos),
+                         WorldToScreen(lane.shape.end+camPos))
   
   pygame.display.flip()
   fpsClock.tick(fps)
